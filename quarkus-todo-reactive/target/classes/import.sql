dc13733e66519aaeb0f37ec8c3770509a20c3e19
Daniel Oh <doh@redhat.com>
2023-07-05 11:51:39 -0400

    Update Quarkus 3.2.0

--- quarkus-todo-reactive/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (1, 'Introduction to Quarkus - Reactive', false, 1, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (2, 'The Reactive Powers of Quarkus', false, 1, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (3, 'Hibernate Reactive with Panache', false, 1, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (4, 'Find more demos on Daniel Oh TV', false, 3, 'https://bit.ly/danielohtv');
-ALTER SEQUENCE todo_seq RESTART WITH 5;
