e69ce933aa29d6dc8c04ba63ba66f35fcdb919d8
Daniel Oh <doh@redhat.com>
2024-02-29 08:24:32 +0100

    Update 3.8.1

--- quarkus-todo/src/test/java/io/quarkus/sample/TodoResourceTest.java
+++ quarkus-todo/src/test/java/io/quarkus/sample/TodoResourceTest.java
@@ -20,105 +20,105 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TodoResourceTest {
 
-    @Test
-    @Order(1)
-    void testInitialItems() {
-        List<Todo> todos = get("/api").then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .extract().body().as(getTodoTypeRef());
-        assertEquals(4, todos.size());
+        @Test
+        @Order(1)
+        void testInitialItems() {
+                List<Todo> todos = get("/api").then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .extract().body().as(getTodoTypeRef());
+                assertEquals(4, todos.size());
 
-        get("/api/1").then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body("title", is("Introduction to Quarkus - Imperative"))
-                .body("completed", is(false));
-    }
+                get("/api/1").then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body("title", is("Introduction to Quarkus - Imperative"))
+                                .body("completed", is(false));
+        }
 
-    @Test
-    @Order(2)
-    void testAddingAnItem() {
-        Todo todo = new Todo();
-        todo.title = "testing the application";
-        given()
-                .body(todo)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .when()
-                .post("/api")
-                .then()
-                .statusCode(HttpStatus.SC_CREATED)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body("title", is(todo.title))
-                .body("completed", is(false))
-                .body("id", is(5));
+        @Test
+        @Order(2)
+        void testAddingAnItem() {
+                Todo todo = new Todo();
+                todo.title = "testing the application";
+                given()
+                                .body(todo)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                                .when()
+                                .post("/api")
+                                .then()
+                                .statusCode(HttpStatus.SC_CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body("title", is(todo.title))
+                                .body("completed", is(false))
+                                .body("id", is(5));
 
-        List<Todo> todos = get("/api").then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .extract().body().as(getTodoTypeRef());
-        assertEquals(5, todos.size());
-    }
+                List<Todo> todos = get("/api").then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .extract().body().as(getTodoTypeRef());
+                assertEquals(5, todos.size());
+        }
 
-    @Test
-    @Order(3)
-    void testUpdatingAnItem() {
-        Todo todo = new Todo();
-        todo.title = "testing the application (updated)";
-        todo.completed = true;
-        given()
-                .body(todo)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .pathParam("id", 5)
-                .when()
-                .patch("/api/{id}")
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body("title", is(todo.title))
-                .body("completed", is(true))
-                .body("id", is(5));
-    }
+        @Test
+        @Order(3)
+        void testUpdatingAnItem() {
+                Todo todo = new Todo();
+                todo.title = "testing the application (updated)";
+                todo.completed = true;
+                given()
+                                .body(todo)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                                .pathParam("id", 5)
+                                .when()
+                                .patch("/api/{id}")
+                                .then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body("title", is(todo.title))
+                                .body("completed", is(true))
+                                .body("id", is(5));
+        }
 
-    @Test
-    @Order(4)
-    void testDeletingAnItem() {
-        given()
-                .contentType(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .pathParam("id", 5)
-                .when()
-                .delete("/api/{id}")
-                .then()
-                .statusCode(HttpStatus.SC_NO_CONTENT);
+        @Test
+        @Order(4)
+        void testDeletingAnItem() {
+                given()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                                .pathParam("id", 5)
+                                .when()
+                                .delete("/api/{id}")
+                                .then()
+                                .statusCode(HttpStatus.SC_NO_CONTENT);
 
-        List<Todo> todos = get("/api").then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .extract().body().as(getTodoTypeRef());
-        assertEquals(4, todos.size());
-    }
+                List<Todo> todos = get("/api").then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .extract().body().as(getTodoTypeRef());
+                assertEquals(4, todos.size());
+        }
 
-    @Test
-    @Order(5)
-    void testDeleteCompleted() {
-        delete("/api")
-                .then()
-                .statusCode(HttpStatus.SC_NO_CONTENT);
+        @Test
+        @Order(5)
+        void testDeleteCompleted() {
+                delete("/api")
+                                .then()
+                                .statusCode(HttpStatus.SC_NO_CONTENT);
 
-        List<Todo> todos = get("/api").then()
-                .statusCode(HttpStatus.SC_OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .extract().body().as(getTodoTypeRef());
-        assertEquals(4, todos.size());
-    }
+                List<Todo> todos = get("/api").then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .extract().body().as(getTodoTypeRef());
+                assertEquals(4, todos.size());
+        }
 
-    private TypeRef<List<Todo>> getTodoTypeRef() {
-        return new TypeRef<List<Todo>>() {
-            // Kept empty on purpose
-        };
-    }
+        private TypeRef<List<Todo>> getTodoTypeRef() {
+                return new TypeRef<List<Todo>>() {
+                        // Kept empty on purpose
+                };
+        }
 
 }
